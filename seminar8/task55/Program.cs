f0e298206c5d50fec1e8599fe520954d20d25678
SteelPlated <mix.singatulin@yandex.ru>
2022-06-08 18:30:24 +0500

    1

---
+++ seminar8/task55/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 55: Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя.*/
+
+/// Создание случайного двумерного массива
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random arrayRandomValue = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = arrayRandomValue.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+/// Mетод печати массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите колличесвто строк: ");
+int userRow = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите колличество столбцов: ");
+int userColumn = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(userRow, userColumn);
+PrintArray(array);
+
+int[,] ChangeArrayString(int[,] array)
+{
+    int[,] flipArray = new int[userRow, userColumn];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            flipArray[i, j] = array[j, i];
+        }
+    }
+    return flipArray;
+}
+
+int[,] flipArray = ChangeArrayString(array);
+Console.WriteLine();
+PrintArray(flipArray);
